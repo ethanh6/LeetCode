d02e6249f29f6b2f0156ad2270acd13073591d0d
ethanhuang <ethanhuang0606@gmail.com>
2022-07-30 17:01:28 +0800

    add 72

---
+++ 72.edit-distance/72.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include <assert.h>
+using namespace std;
+
+class Solution {
+public:
+  int minDistance(string word1, string word2) {
+    return 0;
+  }
+};
+
+int main() {
+
+  Solution sol;
+  int ans;
+  string w1, w2;
+
+  ans = sol.minDistance(w1, w2);
+  assert(ans == 3);
+
+  return 0;
+}
