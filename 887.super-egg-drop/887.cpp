dee9ed774d7388bcfbbf5235ee538e5a1d63e4bd
ethanhuang <ethanhuang0606@gmail.com>
2022-08-07 19:27:31 +0800

    add 10 & 887

---
+++ 887.super-egg-drop/887.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include <assert.h>
+using namespace std;
+
+int main() {
+
+  Solution sol;
+  string ans;
+
+  ans = sol.SampleFunc();
+  assert(ans == xxx);
+
+  return 0;
+}
---
+++ unfinished.md
@@ -0,0 +1,4 @@
+# List of unfinished question's
+
+
+1. 10 - regular expression matching (hard)
