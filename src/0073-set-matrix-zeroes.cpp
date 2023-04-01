54fd15463b1ef1cc5e2473bde1c3a8982cf369d6
ethanhuang <ethanhuang0606@gmail.com>
2023-03-31 22:34:35 -0700

    add 73

--- src/0073-set-matrix-zeroes.cpp
+++ src/0073-set-matrix-zeroes.cpp
@@ -35,6 +35,7 @@ You must do it [in place](https://en.wikipedia.org/wiki/In-place_algorithm).
   * A simple improvement uses `O(m + n)` space, but still not the best solution.
   * Could you devise a constant space solution?
 
+  i
 
 */
 
@@ -44,20 +45,18 @@ class Solution {
 public:
   void setZeroes(vector<vector<int>> &matrix) {
     int n = matrix.size(), m = matrix[0].size();
+
+    // edge case: first col and row need to be set to zero.
     bool setFirstRow = false, setFirstCol = false;
 
     // set flags
     for (int j = 0; j < m; ++j)
-      if (matrix[0][j] == 0) {
+      if (matrix[0][j] == 0)
         setFirstRow = true;
-        matrix[0][j] = 0;
-      }
 
     for (int i = 0; i < n; ++i)
-      if (matrix[i][0] == 0) {
+      if (matrix[i][0] == 0)
         setFirstCol = true;
-        matrix[i][0] = 0;
-      }
 
     for (int i = 1; i < n; ++i)
       for (int j = 1; j < m; ++j)
@@ -72,13 +71,22 @@ public:
         if (matrix[i][0] == 0 || matrix[0][j] == 0)
           matrix[i][j] = 0;
 
-    if (matrix[0][0] == 0 || setFirstRow)
+    if (matrix[0][0] == 0) {
       for (int i = 0; i < m; ++i)
         matrix[0][i] = 0;
 
-    if (matrix[0][0] == 0 || setFirstCol)
-      for (int i = 0; i < n; ++i)
-        matrix[i][0] = 0;
+      for (int j = 0; j < n; ++j)
+        matrix[j][0] = 0;
+
+    } else {
+      if (setFirstRow) 
+        for (int i = 0; i < m; ++i)
+          matrix[0][i] = 0;
+
+      if (setFirstCol) 
+        for (int i = 0; i < n; ++i)
+          matrix[i][0] = 0;
+    }
   }
 };
 
@@ -94,6 +102,31 @@ TEST_CASE("set-matrix-zeroes - 1", "[0073]") {
   Solution sol;
   auto matrix = vector<vector<int>>{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
   auto output = vector<vector<int>>{{0, 0, 0, 0}, {0, 4, 5, 0}, {0, 3, 1, 0}};
+  sol.setZeroes(matrix);
+  CHECK(matrix == output);
+}
+
+TEST_CASE("set-matrix-zeroes - 2", "[0073]") {
+  Solution sol;
+  auto matrix = vector<vector<int>>{
+      {INT_MAX, 6, -7, 0}, {6, -8, -6, 0}, {2, -9, -6, -10}};
+  auto output = vector<vector<int>>{{0, 0, 0, 0}, {0, 0, 0, 0}, {2, -9, -6, 0}};
+  sol.setZeroes(matrix);
+  CHECK(matrix == output);
+}
+
+TEST_CASE("set-matrix-zeroes - 3", "[0073]") {
+  Solution sol;
+  auto matrix = vector<vector<int>> {{8, 3, 6, 9, 7, 8, 0, 6},
+                                     {0, 3, 7, 0, 0, 4, 3, 8},
+                                     {5, 3, 6, 7, 1, 6, 2, 6},
+                                     {8, 7, 2, 5, 0, 6, 4, 0},
+                                     {0, 2, 9, 9, 3, 9, 7, 3}};
+  auto output = vector<vector<int>>{{0, 0, 0, 0, 0, 0, 0, 0},
+                                    {0, 0, 0, 0, 0, 0, 0, 0},
+                                    {0, 3, 6, 0, 0, 6, 0, 0},
+                                    {0, 0, 0, 0, 0, 0, 0, 0},
+                                    {0, 0, 0, 0, 0, 0, 0, 0}};
   sol.setZeroes(matrix);
   CHECK(matrix == output);
 }
