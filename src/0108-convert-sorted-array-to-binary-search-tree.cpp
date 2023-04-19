d0b50b1208a208cd4a4855a60150adb18e05b53c
ethanhuang <ethanhuang0606@gmail.com>
2023-04-18 17:51:55 -0700

    add 108, 226

---
+++ src/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -0,0 +1,67 @@
+/*
+Given an integer array `nums` where the elements are sorted in **ascending
+order** , convert _it to a_ ** _height-balanced_** _binary search tree_.
+
+ **Example 1:**
+
+![](https://assets.leetcode.com/uploads/2021/02/18/btree1.jpg)
+
+
+
+     **Input:** nums = [-10,-3,0,5,9] **Output:** [0,-3,9,-10,null,5]
+**Explanation:** [0,-10,5,null,-3,null,9] is also
+accepted:![](https://assets.leetcode.com/uploads/2021/02/18/btree2.jpg)
+
+ **Example 2:**
+
+![](https://assets.leetcode.com/uploads/2021/02/18/btree.jpg)
+
+
+
+     **Input:** nums = [1,3] **Output:** [3,1] **Explanation:** [1,null,3] and
+[3,1] are both height-balanced BSTs.
+
+ **Constraints:**
+
+  * `1 <= nums.length <= 104`
+  * `-104 <= nums[i] <= 104`
+  * `nums` is sorted in a **strictly increasing** order.
+
+
+*/
+
+#include "leetcode.hpp"
+
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  TreeNode *helper(int lo, int hi) {
+      if (lo > hi)
+        return nullptr;
+
+      int mid = lo + (hi - lo) / 2;
+
+      TreeNode *root = new TreeNode(nums[mid], helper(lo, mid - 1), helper(mid + 1, hi));
+
+      return root;
+  }
+
+  TreeNode *sortedArrayToBST(vector<int> &nums) {
+    return helper(0, nums.size() - 1);
+  }
+};
+
+TEST_CASE("convert-sorted-array-to-binary-search-tree", "[0108]") {
+  // Solution sol;
+  CHECK(true);
+}
---
+++ src/0226-invert-binary-tree.cpp
@@ -0,0 +1,86 @@
+/*
+Given the `root` of a binary tree, invert the tree, and return _its root_.
+
+ **Example 1:**
+
+![](https://assets.leetcode.com/uploads/2021/03/14/invert1-tree.jpg)
+
+
+
+     **Input:** root = [4,2,7,1,3,6,9] **Output:** [4,7,2,9,6,3,1]
+
+ **Example 2:**
+
+![](https://assets.leetcode.com/uploads/2021/03/14/invert2-tree.jpg)
+
+
+
+     **Input:** root = [2,1,3] **Output:** [2,3,1]
+
+ **Example 3:**
+
+
+
+     **Input:** root = [] **Output:** []
+
+ **Constraints:**
+
+  * The number of nodes in the tree is in the range `[0, 100]`.
+  * `-100 <= Node.val <= 100`
+
+
+*/
+
+#include "leetcode.hpp"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution_recursion {
+public:
+  TreeNode *invertTree(TreeNode *root) {
+    if (!root)
+      return nullptr;
+
+    invertTree(root->left);
+    invertTree(root->right);
+    swap(root->left, root->right);
+    return root;
+  }
+};
+
+class Solution_iterative {
+public:
+  TreeNode *invertTree(TreeNode *root) {
+    if (!root)
+      return nullptr;
+
+    queue<TreeNode *> q;
+    q.push(root);
+
+    for (; !q.empty(); q.pop()) {
+      auto cur = q.front();
+      if (!cur)
+        continue;
+      swap(cur->left, cur->right);
+      q.push(cur->left);
+      q.push(cur->right);
+    }
+
+    return root;
+  }
+};
+
+TEST_CASE("invert-binary-tree", "[0226]") {
+  Solution sol;
+  CHECK(true);
+}
