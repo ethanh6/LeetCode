1ece5805a8b9ba5021bfe64cde203b44e622da42
ethanhuang <ethanhuang0606@gmail.com>
2023-05-01 16:58:10 -0700

    add 141, 206

---
+++ src/0141-linked-list-cycle.cpp
@@ -0,0 +1,76 @@
+/*
+Given `head`, the head of a linked list, determine if the linked list has a
+cycle in it.
+
+ **Example 1:**
+
+![](https://assets.leetcode.com/uploads/2018/12/07/circularlinkedlist.png)
+
+
+
+     **Input:** head = [3,2,0,-4], pos = 1 **Output:** true **Explanation:**
+There is a cycle in the linked list, where the tail connects to the 1st node
+(0-indexed).
+
+ **Example 2:**
+
+![](https://assets.leetcode.com/uploads/2018/12/07/circularlinkedlist_test2.png)
+
+
+
+     **Input:** head = [1,2], pos = 0 **Output:** true **Explanation:** There is
+a cycle in the linked list, where the tail connects to the 0th node.
+
+ **Example 3:**
+
+![](https://assets.leetcode.com/uploads/2018/12/07/circularlinkedlist_test3.png)
+
+
+
+     **Input:** head = [1], pos = -1 **Output:** false **Explanation:** There is
+no cycle in the linked list.
+
+ **Constraints:**
+
+  * The number of the nodes in the list is in the range `[0, 104]`.
+  * `-105 <= Node.val <= 105`
+  * `pos` is `-1` or a **valid index** in the linked-list.
+
+ **Follow up:** Can you solve it using `O(1)` (i.e. constant) memory?
+
+
+*/
+
+#include "leetcode.hpp"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  bool hasCycle(ListNode *head) {
+    if (!head)
+      return false;
+
+    for (ListNode *slow = head, *fast = head->next;
+         slow != fast;
+         slow = slow->next, fast = fast->next->next)
+
+      // terminal condition
+      // when fast reaches end first
+      if (!fast or !fast->next)
+        return false;
+
+    return true;
+  }
+};
+
+TEST_CASE("linked-list-cycle", "[0141]") {
+  Solution sol;
+  CHECK(true);
+}
---
+++ src/0206-reverse-linked-list.cpp
@@ -0,0 +1,83 @@
+/*
+Given the `head` of a singly linked list, reverse the list, and return _the
+reversed list_.
+
+ **Example 1:**
+
+![](https://assets.leetcode.com/uploads/2021/02/19/rev1ex1.jpg)
+
+
+
+     **Input:** head = [1,2,3,4,5] **Output:** [5,4,3,2,1]
+
+ **Example 2:**
+
+![](https://assets.leetcode.com/uploads/2021/02/19/rev1ex2.jpg)
+
+
+
+     **Input:** head = [1,2] **Output:** [2,1]
+
+ **Example 3:**
+
+
+
+     **Input:** head = [] **Output:** []
+
+ **Constraints:**
+
+  * The number of nodes in the list is the range `[0, 5000]`.
+  * `-5000 <= Node.val <= 5000`
+
+ **Follow up:** A linked list can be reversed either iteratively or
+recursively. Could you implement both?
+
+
+*/
+
+#include "leetcode.hpp"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution_recursive {
+public:
+  ListNode *reverse(ListNode *node, ListNode *prev) {
+    if (!node)
+      return prev;
+    ListNode *next = node->next;
+    node->next = prev;
+    return reverse(next, node);
+  }
+
+  ListNode *reverseList(ListNode *head) { return reverse(head, nullptr); }
+};
+
+class Solution {
+public:
+  ListNode *reverseList(ListNode *head) {
+
+    ListNode *prev = nullptr;
+    ListNode *ptr = head;
+
+    while (ptr) {
+      ListNode *next = ptr->next; // preserve the rest of the list
+      ptr->next = prev;
+      prev = ptr;
+      ptr = next;
+    }
+
+    return ptr;
+  }
+};
+TEST_CASE("reverse-linked-list", "[0206]") {
+  Solution sol;
+  CHECK(true);
+}
