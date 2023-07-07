e95f944d2b3f335b0b38c5628d3e40c698ab03af
ethanhuang <ethanhuang0606@gmail.com>
2023-07-06 17:40:58 -0700

    update 55

--- src/0055-jump-game.cpp
+++ src/0055-jump-game.cpp
@@ -30,7 +30,7 @@ makes it impossible to reach the last index.
 
 #include "leetcode.hpp"
 
-class Solution {
+class Solution0 {
 public:
   bool canJump(vector<int> &nums) {
     int n = nums.size();
@@ -43,6 +43,29 @@ public:
     }
 
     return last_reachable == 0;
+  }
+};
+
+class Solution {
+public:
+  bool canJump(vector<int> &nums) {
+    int n = nums.size();
+
+    vector<bool> dp(n, false);
+    dp[n - 1] = true;
+
+    for (int i = n - 2; i >= 0; --i) {
+      // can only jump to index (n - 1)
+      int furthestJump = min(i + nums[i], n - 1);
+
+      for (int j = i + 1; j <= furthestJump; ++j) {
+        if (dp[j]) {
+          dp[i] = true;
+          break;
+        }
+      }
+    }
+    return dp[0];
   }
 };
 
