e33c2e69ef9e2a3d23382d7c2a4e45f06ba707e1
ethanhuang <ethanhuang0606@gmail.com>
2023-07-23 16:11:19 -0700

    add 88, 27

---
+++ src/0027-remove-element.cpp
@@ -0,0 +1,76 @@
+/*
+Given an integer array `nums` and an integer `val`, remove all occurrences of
+`val` in `nums` [**in-place**](https://en.wikipedia.org/wiki/In-
+place_algorithm). The order of the elements may be changed. Then return _the
+number of elements in_`nums` _which are not equal to_`val`.
+
+Consider the number of elements in `nums` which are not equal to `val` be `k`,
+to get accepted, you need to do the following things:
+
+  * Change the array `nums` such that the first `k` elements of `nums` contain
+the elements which are not equal to `val`. The remaining elements of `nums` are
+not important as well as the size of `nums`.
+  * Return `k`.
+
+ **Custom Judge:**
+
+The judge will test your solution with the following code:
+
+
+
+    int[] nums = [...]; // Input arrayint val = ...; // Value to removeint[]
+expectedNums = [...]; // The expected answer with correct length. // It is
+sorted with no values equaling val.int k = removeElement(nums, val); // Calls
+your implementationassert k == expectedNums.length;sort(nums, 0, k); // Sort the
+first k elements of numsfor (int i = 0; i < actualLength; i++) {    assert
+nums[i] == expectedNums[i];}
+
+If all assertions pass, then your solution will be **accepted**.
+
+ **Example 1:**
+
+
+
+     **Input:** nums = [3,2,2,3], val = 3 **Output:** 2, nums = [2,2,_,_]
+**Explanation:** Your function should return k = 2, with the first two elements
+of nums being 2.It does not matter what you leave beyond the returned k (hence
+they are underscores).
+
+ **Example 2:**
+
+
+
+     **Input:** nums = [0,1,2,2,3,0,4,2], val = 2 **Output:** 5, nums =
+[0,1,4,0,3,_,_,_] **Explanation:** Your function should return k = 5, with the
+first five elements of nums containing 0, 0, 1, 3, and 4.Note that the five
+elements can be returned in any order.It does not matter what you leave beyond
+the returned k (hence they are underscores).
+
+ **Constraints:**
+
+  * `0 <= nums.length <= 100`
+  * `0 <= nums[i] <= 50`
+  * `0 <= val <= 100`
+
+
+*/
+
+#include "leetcode.hpp"
+
+class Solution {
+public:
+  int removeElement(vector<int> &nums, int val) {
+    int i = 0;
+    for (const auto &n : nums) {
+      if (n != val) {
+        nums[i++] = n;
+      }
+    }
+    return i;
+  }
+}
+
+TEST_CASE("remove-element", "[0027]") {
+  Solution sol;
+  CHECK(true);
+}
