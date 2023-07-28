91b69539bbb8961b6b7d7436fff80eee73f8ee3a
ethanhuang <ethanhuang0606@gmail.com>
2023-07-27 19:39:45 -0700

    add 274

---
+++ src/0274-h-index.cpp
@@ -0,0 +1,55 @@
+/*
+Given an array of integers `citations` where `citations[i]` is the number of
+citations a researcher received for their `ith` paper, return _the researcher
+'s h-index_.
+
+According to the [definition of h-index on
+Wikipedia](https://en.wikipedia.org/wiki/H-index): The h-index is defined as
+the maximum value of `h` such that the given researcher has published at least
+`h` papers that have each been cited at least `h` times.
+
+ **Example 1:**
+
+
+
+     **Input:** citations = [3,0,6,1,5] **Output:** 3 **Explanation:**
+[3,0,6,1,5] means the researcher has 5 papers in total and each of them had
+received 3, 0, 6, 1, 5 citations respectively.Since the researcher has 3 papers
+with at least 3 citations each and the remaining two with no more than 3
+citations each, their h-index is 3.
+
+ **Example 2:**
+
+
+
+     **Input:** citations = [1,3,1] **Output:** 1
+
+ **Constraints:**
+
+  * `n == citations.length`
+  * `1 <= n <= 5000`
+  * `0 <= citations[i] <= 1000`
+
+
+*/
+
+#include "leetcode.hpp"
+
+class Solution {
+public:
+  int hIndex(vector<int> &citations) {
+    // sort in decending order
+    sort(citations.begin(), citations.end(), greater<int>());
+
+    for (size_t i{}; i < citations.size(); ++i) {
+      if (i >= citations[i])
+        return i;
+    }
+    return citations.size();
+  }
+};
+
+TEST_CASE("h-index", "[0274]") {
+  Solution sol;
+  CHECK(true);
+}
